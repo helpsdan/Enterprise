18cfa6f395bd86b1ecd23f75f29d38841b020bc6
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-03 22:41:36 -0300

    adding my first lesson about c#

---
+++ 01.Fiap.HelloWorld/01.Fiap.HelloWorld.UI/Program.cs
@@ -0,0 +1,21 @@
+﻿using _01.Fiap.HelloWorld.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fiap.HelloWorld.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciar um aluno
+            Aluno aluno = new Aluno();
+            aluno.Nome = "Jefty"; //Set
+            Console.WriteLine(aluno.Nome);//Get
+        }
+
+    }
+}
