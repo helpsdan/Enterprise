b5bc01fc4cc2d7146c0a9f9a672474c5745d3864
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-13 20:58:22 -0300

    Adicionando aula de MVC on c#

---
+++ 03.Fiap.Web.MVC/03.Fiap.Web.MVC/Controllers/MultaController.cs
@@ -0,0 +1,29 @@
+﻿using _03.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03.Fiap.Web.MVC.Controllers
+{
+    public class MultaController : Controller
+    {
+        [HttpGet]//abrir a página de formulario
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]//recuperar dados e cadastrar no db
+        public ActionResult Cadastrar(Multa multa)
+        {
+
+            ViewBag.churros = multa.Placa;
+            TempData["msg"] = "Multa Cadastrada!";
+            return View(multa);
+            // exibe a placa na tela
+            //return Content(multa.Placa);
+        }
+    }
+}
