3fba4b773c3023158dce2ad637b3a2fed7b4bf3d
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-27 18:56:25 -0300

    adicionando aulas de segunda e sexta

---
+++ Fiap06.Web.MVC/Fiap06.Web.MVC/Controllers/BebidaController.cs
@@ -0,0 +1,16 @@
+﻿using Fiap06.Web.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap06.Web.MVC.Controllers
+{
+    public class BebidaController : Controller
+    {
+        private BotecoContext _context = new BotecoContext();
+
+
+    }
+}
